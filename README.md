01cf17089970c71ef8ad2d569b856c4c3a6afd14
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-02-27 00:49:24 -0300

    update github workflow and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# http-injector_server-Docker
+# Its functionality
+
+Run a server with SSH and Squid completely high user availability.
+
+with some files you already have access to a great service 24 hours with low drop in use*
--- scripts/badvpn.sh
+++ scripts/badvpn.sh
@@ -35,7 +35,7 @@ fun_udp1 () {
                 echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -L -Logfile /tmp/badvpn_log.txt -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10 --client-socket-sndbuf 10000" >> /etc/autostart
             } || {
                 sed -i '/udpvpn/d' /etc/autostart
-                echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10 --client-socket-sndbuf 10000" >> /etc/autostart
+                echo -e "ps x | grep 'udpvpn' | grep -v 'grep' || screen -L -Logfile /tmp/badvpn_log.txt -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 10000 --max-connections-for-client 10 --client-socket-sndbuf 10000" >> /etc/autostart
             }
             sleep 1
         }
