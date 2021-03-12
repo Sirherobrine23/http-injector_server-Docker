1456d66d059ad4f4a701a38f03021d5c9568a8de
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-03-12 00:52:35 -0300

    Update wireguard_start.sh

--- scripts/wireguard_start.sh
+++ scripts/wireguard_start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo "Wireguard start Tunnel wg0"
-wg-quick up wg0
+source /etc/wireguard/params
+echo "Wireguard start Tunnel ${SERVER_WG_NIC}"
+ip link add dev ${SERVER_WG_NIC} type wireguard
+
+ip link set up dev ${SERVER_WG_NIC}
+printf 'nameserver %s\n' '8.8.8.8' | resolvconf -a tun.${SERVER_WG_NIC} -m 0 -x
+
+sysctl -w net.ipv4.conf.all.rp_filter=2
