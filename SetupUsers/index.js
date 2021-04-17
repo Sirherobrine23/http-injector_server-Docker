23028dfeade260269991569ac566231633a46809
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-04-16 23:02:02 -0300

    Push GUI Web Config

--- SetupUsers/index.js
+++ SetupUsers/index.js
@@ -4,20 +4,58 @@ const fs = require("fs");
 const path = require("path");
 var cors = require("cors");
 const bodyParser = require("body-parser");
+const { resolve } = require("path");
+const app = express();
 
-function api(port_api){
-    const app = express();
-    app.use(fileUpload({limits: { fileSize: 512 * 1024 }}));
-    app.use(cors());
-    app.use(bodyParser.json()); /* https://github.com/github/fetch/issues/323#issuecomment-331477498 */
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(rateLimit({windowMs: 15 * 60 * 1000, /* 15 minutes */ max: 1000 /* limit each IP to 100 requests per windowMs */}));
-    // -----------------------------
+app.use(cors());
+app.use(bodyParser.json()); /* https://github.com/github/fetch/issues/323#issuecomment-331477498 */
+app.use(bodyParser.urlencoded({ extended: true }));
+// ******************************************************
+const tokenFile = resolve(__dirname, "token.txt");
+const ConfigFile = resolve((process.env.USERPROFILE||process.env.HOME), "HttpInjectorConfig.json");
+require("crypto").randomBytes(Math.random() + 1 * 1000, function(err, buffer) {
+    if (err) console.warn(err);
+    const new_token = buffer.toString("hex");
+    fs.writeFileSync(tokenFile, new_token)
+    
+})
+// -----------------------------
+app.get("/", (req, res) => {
+    res.sendFile(resolve(__dirname, "pages", "index.html"))
+});
+app.get("/login", (req, res) => {
+    res.sendFile(resolve(__dirname, "pages", "login.html"))
+});
+app.post("/get_token", (req, res) => {
+    const body = req.headers
+    if (body.user !== process.env.USERNAME) return res.status(400).json({
+        "messsage": "Check username"
+    })
+    if (body.pass !== process.env.PASSWORLD) return res.status(400).json({
+        "messsage": "Check username"
+    })
+    res.send(fs.readFileSync(tokenFile, "utf8"))
+});
 
-    // -----------------------------
-    const port = (port_api||1112)
-    app.listen(port, function (){
-        console.log(`Web Config: ${port}`);
-    });
-    return port
-}
+app.post("/check_token", (req, res) => {
+    const body = req.headers;
+    const localToken = fs.readFileSync(tokenFile, "utf8")
+    if (localToken === body.token) res.send("ok")
+    else res.sendStatus(403)
+});
+
+app.post("/save_config", (req, res) => {
+    const body = req.headers
+    const localToken = fs.readFileSync(tokenFile, "utf8")
+    if (localToken === body.token) {
+        const parseConfig = JSON.stringify(JSON.parse(Buffer.from(body.injectorconfig, "base64").toString()), null, 4)
+        fs.writeFileSync(ConfigFile, parseConfig)
+    }
+    else res.sendStatus(404)
+});
+// -----------------------------
+const port = (process.env.port_api||1112)
+app.listen(port, function (){
+    console.log(`Web Config: ${port}`);
+});
+return port
